cf4dd33fb43eb90e3523a86e8340abc93805e836
Karamchi <karamchi.ghandi@gmail.com>
2022-11-24 21:32:05 -0300

    Search history

--- app/src/main/java/org/evcs/android/activity/search/SearchActivity.kt
+++ app/src/main/java/org/evcs/android/activity/search/SearchActivity.kt
@@ -33,8 +33,12 @@ class SearchActivity : BaseActivity2(), SearchActivityView {
     private lateinit var mRecycler: EndlessRecyclerView
     private lateinit var mFailView: LinearLayout
     private lateinit var mEmptyView: TextView
-
     private lateinit var mAddress: String
+
+    private lateinit var mHistoryRecycler: EndlessRecyclerView
+    private lateinit var mHistoryAdapter: SearchResultsAdapter
+    private lateinit var mHistoryLayout: View
+    private lateinit var mHistoryClear: TextView
 
     override fun inflate(layoutInflater: LayoutInflater?): View {
         val binding = ActivitySearchBinding.inflate(layoutInflater!!)
@@ -42,6 +46,9 @@ class SearchActivity : BaseActivity2(), SearchActivityView {
         mFailView = binding.activitySearchFail
         mEmptyView = binding.activitySearchEmpty
         mLoading = binding.activitySearchLoading
+        mHistoryRecycler = binding.activitySearchHistory
+        mHistoryLayout = binding.activitySearchHistoryLayout
+        mHistoryClear = binding.activitySearchHistoryClear
         return binding.root
     }
 
@@ -67,6 +74,8 @@ class SearchActivity : BaseActivity2(), SearchActivityView {
         mRecycler.adapter = mAdapter
         val linearLayoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
         mRecycler.layoutManager = linearLayoutManager
+        mHistoryRecycler.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        refreshHistoryView()
     }
 
     override fun setListeners() {
@@ -84,7 +93,7 @@ class SearchActivity : BaseActivity2(), SearchActivityView {
                 this@SearchActivity.finish()
             }
         })
-        mAdapter.setItemClickListener(object : BaseRecyclerAdapterItemClickListener<Location>() {
+        val itemClickListener = object : BaseRecyclerAdapterItemClickListener<Location>() {
             override fun onItemClicked(item: Location?, adapterPosition: Int) {
                 saveToLocationHistory(item!!)
                 val data = Intent()
@@ -92,15 +101,33 @@ class SearchActivity : BaseActivity2(), SearchActivityView {
                 setResult(RESULT_OK, data)
                 finish()
             }
-        })
+        }
+        mAdapter.setItemClickListener(itemClickListener)
+        mHistoryAdapter.setItemClickListener(itemClickListener)
+        mHistoryClear.setOnClickListener {
+            StorageUtils.storeInSharedPreferences(Extras.SearchActivity.HISTORY, arrayOf<Location>())
+            refreshHistoryView()
+        }
+    }
+
+    private fun refreshHistoryView() {
+        mHistoryAdapter = SearchResultsAdapter()
+        mHistoryRecycler.adapter = mHistoryAdapter
+        mHistoryAdapter.appendBottomAll(getLocationHistory())
+        mEmptyView.visibility = if (mHistoryAdapter.itemCount == 0) View.VISIBLE else View.GONE
     }
 
     private fun saveToLocationHistory(item: Location) {
+        val history = getLocationHistory()
+        history.removeAll { location -> location.latLng == item.latLng }
+        history.add(0, item)
+        StorageUtils.storeInSharedPreferences(Extras.SearchActivity.HISTORY, history.toTypedArray())
+    }
+
+    private fun getLocationHistory(): MutableList<Location> {
         val json = StorageUtils.getStringFromSharedPreferences(Extras.SearchActivity.HISTORY, "")
         val gson: Gson = BaseGsonBuilder.getBaseGsonBuilder().create()
-        val history = gson.fromJson(json, Array<Location>::class.java)?.toMutableList() ?: mutableListOf()
-        history.add(item)
-        StorageUtils.storeInSharedPreferences(Extras.SearchActivity.HISTORY, history.toTypedArray())
+        return gson.fromJson(json, Array<Location>::class.java)?.toMutableList() ?: mutableListOf()
     }
 
     private fun onLocationChosen(address: String, latLng: LatLng) {
@@ -116,6 +143,7 @@ class SearchActivity : BaseActivity2(), SearchActivityView {
         hideLoading()
         mEmptyView.visibility = View.GONE
         mFailView.visibility = View.GONE
+        mHistoryLayout.visibility = View.GONE
 
         if (onFirstPage) {
             mAdapter.clear()
@@ -129,15 +157,17 @@ class SearchActivity : BaseActivity2(), SearchActivityView {
     override fun onEmptyResponse() {
         hideLoading()
         mEmptyView.visibility = View.GONE
+        mHistoryLayout.visibility = View.GONE
         (mFailView.getChildAt(0) as TextView).text = "Cannot find \"" + mAddress + "\""
         mFailView.visibility = View.VISIBLE
     }
 
-    //This will be "show history"
+    //show history
     private fun clear() {
         mAdapter.clear()
         mEmptyView.visibility = View.VISIBLE
         mFailView.visibility = View.GONE
+        mHistoryLayout.visibility = View.VISIBLE
     }
 
     override fun showLoading() {

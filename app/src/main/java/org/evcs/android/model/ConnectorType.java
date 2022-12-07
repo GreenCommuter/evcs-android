f010a89df60672f14be6e1e297b7da5a89517c21
Karamchi <karamchi.ghandi@gmail.com>
2022-12-07 20:10:19 -0300

    Connector assets

--- app/src/main/java/org/evcs/android/model/ConnectorType.java
+++ app/src/main/java/org/evcs/android/model/ConnectorType.java
@@ -7,12 +7,12 @@ import org.evcs.android.R;
 import java.io.Serializable;
 
 public enum ConnectorType implements Serializable {
-    TESLA("Tesla", R.drawable.ic_map_pin_selected, "Dc"),
-    CHADEMO("CHAdeMO", R.drawable.ic_map_pin_selected, "Dc"),
-    TYPE1("Type 1(J-1772)", R.drawable.ic_map_pin_selected, "Ac"),
-    TYPE2("Type 2", R.drawable.ic_map_pin_selected, "Ac"),
-    CCS1("CCS1", R.drawable.ic_map_pin_selected, "Ac"),
-    CCS2("CCS2", R.drawable.ic_map_pin_selected, "Ac");
+    TESLA("Tesla", R.drawable.conn_tesla, "Dc"),
+    CHADEMO("CHAdeMO", R.drawable.conn_chademo, "Dc"),
+    TYPE1("Type 1(J-1772)", R.drawable.conn_type1_j1772, "Ac"),
+    TYPE2("Type 2", R.drawable.conn_type2, "Ac"),
+    CCS1("CCS1", R.drawable.conn_ccs1, "Ac"),
+    CCS2("CCS2", R.drawable.conn_ccs2, "Ac");
 
     private final String mString;
     public final @DrawableRes int mIcon;

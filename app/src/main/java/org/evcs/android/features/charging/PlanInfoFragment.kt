1cba84bc4478e882f08563092c82dc4128bbd319
Karamchi <karamchi.ghandi@gmail.com>
2023-10-05 12:49:06 -0300

    Loading

--- app/src/main/java/org/evcs/android/features/charging/PlanInfoFragment.kt
+++ app/src/main/java/org/evcs/android/features/charging/PlanInfoFragment.kt
@@ -86,6 +86,7 @@ class PlanInfoFragment : ErrorFragment<PlanInfoPresenter>(), PlanInfoView {
         }
 
         setProgress(status)
+        mBinding.planInfoChargeRate.setParentVisibility(true)
 
         if (status == null) {
             showPlanDialog(mExplorePlansText, false, null)
@@ -158,7 +159,7 @@ class PlanInfoFragment : ErrorFragment<PlanInfoPresenter>(), PlanInfoView {
     }
 
     override fun showChargeRate(rate: String) {
-        mBinding.planInfoChargeRate.visibility = View.VISIBLE
+        mBinding.planInfoChargeRateLoading.isVisible = false
         mBinding.planInfoChargeRate.setText(rate)
     }
 

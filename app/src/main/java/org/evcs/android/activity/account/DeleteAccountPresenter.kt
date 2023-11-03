d59125a1ed4f2bb2930b9d800c015b327a6ca942
Karamchi <karamchi.ghandi@gmail.com>
2023-11-03 10:38:50 -0300

    Delete account working

--- app/src/main/java/org/evcs/android/activity/account/DeleteAccountPresenter.kt
+++ app/src/main/java/org/evcs/android/activity/account/DeleteAccountPresenter.kt
@@ -2,13 +2,13 @@ package org.evcs.android.activity.account
 
 import com.base.networking.retrofit.RetrofitServices
 import okhttp3.ResponseBody
-import org.evcs.android.model.ClientSecret
 import org.evcs.android.model.shared.RequestError
 import org.evcs.android.network.callback.AuthCallback
-import org.evcs.android.network.service.PaymentMethodsService
 import org.evcs.android.network.service.UserService
 import org.evcs.android.network.service.presenter.ServicesPresenter
 import org.evcs.android.util.ErrorUtils
+import org.evcs.android.util.StorageUtils
+import org.evcs.android.util.UserUtils
 
 class DeleteAccountPresenter(viewInstance: DeleteAccountView, services: RetrofitServices) :
         ServicesPresenter<DeleteAccountView?>(viewInstance, services) {
@@ -18,6 +18,8 @@ class DeleteAccountPresenter(viewInstance: DeleteAccountView, services: Retrofit
                 .enqueue(object : AuthCallback<Void>(this) {
                     override fun onResponseSuccessful(response: Void?) {
                         view.onAccountDeleted()
+                        UserUtils.logout(null)
+                        StorageUtils.clearKey(UserUtils.USER_EMAIL_PREF)
                     }
 
                     override fun onResponseFailed(responseBody: ResponseBody, code: Int) {

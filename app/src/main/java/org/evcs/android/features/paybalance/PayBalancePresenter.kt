c1dc47233fa75cadf21bd52ed9028204e43acfcf
Karamchi <karamchi.ghandi@gmail.com>
2024-06-10 19:35:42 -0300

    Endpoint

--- app/src/main/java/org/evcs/android/features/paybalance/PayBalancePresenter.kt
+++ app/src/main/java/org/evcs/android/features/paybalance/PayBalancePresenter.kt
@@ -13,12 +13,15 @@ import org.evcs.android.util.ErrorUtils
 class PayBalancePresenter(viewInstance: PayBalanceView, services: RetrofitServices) :
     ServicesPresenter<PayBalanceView?>(viewInstance, services) {
 
+    private lateinit var mPendingPaymentId: String
+
     fun getPendingPayment() {
         getService(PaymentsService::class.java).rejectedPayments.enqueue(
             object : AuthCallback<ArrayList<Payment>>(this) {
                 override fun onResponseSuccessful(response: ArrayList<Payment>) {
                     if (response.size == 1) {
                         view.showPendingPayment(response.first())
+                        mPendingPaymentId = response.first().id
                     } else {
                         Rollbar.instance().warning("More than one payment returned: " + response.size.toString())
                         view.showError(RequestError.getUnknownError())
@@ -36,20 +39,19 @@ class PayBalancePresenter(viewInstance: PayBalanceView, services: RetrofitServic
     }
 
     fun processPayment() {
-        view.onPaymentSuccess()
-//        getService(PaymentsService::class.java).deleteAccount()
-//            .enqueue(object : AuthCallback<Void>(this) {
-//                override fun onResponseSuccessful(response: Void?) {
-//                    view.onAccountDeleted()
-//                }
-//
-//                override fun onResponseFailed(responseBody: ResponseBody, code: Int) {
-//                    view.showError(ErrorUtils.getError(responseBody))
-//                }
-//
-//                override fun onCallFailure(t: Throwable) {
-//                    view.showError(RequestError.getNetworkError())
-//                }
-//            })
+        getService(PaymentsService::class.java).processPayment(mPendingPaymentId)
+            .enqueue(object : AuthCallback<Void>(this) {
+                override fun onResponseSuccessful(response: Void?) {
+                    view.onPaymentSuccess()
+                }
+
+                override fun onResponseFailed(responseBody: ResponseBody, code: Int) {
+                    view.showError(ErrorUtils.getError(responseBody))
+                }
+
+                override fun onCallFailure(t: Throwable) {
+                    view.showError(RequestError.getNetworkError())
+                }
+            })
     }
 }

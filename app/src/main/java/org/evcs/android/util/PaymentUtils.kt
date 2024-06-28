6e8c2522e89247738163382d99498b5dfb77ba66
Karamchi <karamchi.ghandi@gmail.com>
2024-06-28 17:10:36 -0300

    Handle preauth

--- app/src/main/java/org/evcs/android/util/PaymentUtils.kt
+++ app/src/main/java/org/evcs/android/util/PaymentUtils.kt
@@ -1,10 +1,13 @@
 package org.evcs.android.util
 
 import android.content.Context
+import android.view.Gravity
 import androidx.fragment.app.FragmentActivity
 import com.base.core.util.NavigationUtils
 import org.evcs.android.R
+import org.evcs.android.activity.ChargingActivity
 import org.evcs.android.activity.PayBalanceActivity
+import org.evcs.android.features.profile.wallet.WalletActivity
 import org.evcs.android.features.shared.EVCSDialogFragment
 import org.evcs.android.model.shared.RequestError
 
@@ -20,5 +23,22 @@ object PaymentUtils {
                 .withCancelOnClickListener { context.finish() }
                 .addButton(context.getString(R.string.profile_payment_button)) { goToPendingPayment(context) }
                 .show(context.supportFragmentManager)
+    }
+
+    fun showPreauthDialog(context: FragmentActivity, retryCallback: () -> Unit) {
+        EVCSDialogFragment.Builder()
+            .setTitle("Payment")
+            .setSubtitle(context.getString(R.string.plan_info_payment_error_subtitle), Gravity.CENTER)
+            .addButton(context.getString(R.string.start_charging_error_retry), {
+                fragment -> fragment.dismiss()
+                retryCallback.invoke()
+            }, R.style.ButtonK_Blue)
+            .addButton(context.getString(R.string.plan_info_payment_error_update), {
+                context.setResult(ChargingActivity.RESULT_CANCELED_WITH_DIALOG)
+                context.finish()
+                context.startActivity(WalletActivity.buildIntent(context, true))
+            }, R.style.ButtonK_BlueOutline)
+            .withCancelOnClickListener { context.finish() }
+            .show(context.supportFragmentManager)
     }
 }
